6a39d389517e58be9221a7480529ac115d2e008f
MilosPeskan <165726289+MilosPeskan@users.noreply.github.com>
2026-02-06 19:10:20 +0100

    Create information-menu.js
    Manages ui and functions of player info menu ui

---
+++ scripts/ui/information-menu.js
@@ -0,0 +1,52 @@
+import { EXECUTIONER_ID } from "../constants.js";
+import { UiController } from "./ui-controller.js";
+
+export class InfoMenu extends UiController{
+    constructor(rootElement, gameState){
+        super(rootElement),
+        this.gameState = gameState;
+
+        this.initializeElements();
+        this.attachEventListeners();
+    }
+
+    initializeElements(){
+        this.elements = {
+            infoRole: this.rootElement.querySelector("#info-holder"),
+            name: this.rootElement.querySelector("#info-name"),
+            role: this.rootElement.querySelector("#info-role"),
+            alignment: this.rootElement.querySelector("#info-alignment"),
+            desc: this.rootElement.querySelector("#info-desc"),
+            target: this.rootElement.querySelector("#info-target"),
+            backButton: this.rootElement.querySelector("#back-to-manager")
+        }
+    }
+
+    attachEventListeners(){
+        this.addEventListener(this.elements.backButton, "click", ()=>{
+            console.log("ovde uslo");
+            this.onBackClicked?.();
+        })
+    }
+
+    displayInfo(player){
+        this.displaySearched(player);
+
+        if(player.roleId == EXECUTIONER_ID){
+            this.elements.target.style.display = "block";
+            this.elements.target.textContent = `Tvoja meta je: ${this.gameState.getExecutionTarget()}`;
+        }
+    }
+
+    displaySearched(player){
+        this.elements.name.textContent = player.name;
+        this.elements.role.textContent = player.getRoleName();
+        this.elements.alignment.textContent = player.getRoleAlignment();
+        this.elements.desc.textContent = player.getRoleDescription();
+    }
+
+    show(displayType){
+        super.show(displayType);
+        this.elements.target.style.display = "none";
+    }
+}
