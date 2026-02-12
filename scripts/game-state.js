8d45bc9a514a3d3c8a291d46b18de55c9005f536
MilosPeskan <165726289+MilosPeskan@users.noreply.github.com>
2026-02-12 23:55:14 +0100

    Update game-state.js
    added night processing, changed night queue, added action methods

--- scripts/game-state.js
+++ scripts/game-state.js
@@ -1,17 +1,30 @@
-import { CONFIG, LYNCH_MESSAGE, MESSAGES, ORDER_OF_ROLES, ROLE_IDS, WIN_MESSAGE } from './constants.js';
+import { ALIGNMENT, CONFIG, LYNCH_MESSAGE, MESSAGES, ORDER_OF_ROLES, ROLE_IDS, STATUS, WIN_MESSAGE } from './constants.js';
+import { ROLES } from './data.js';
 import { PlayerClass } from './player-manager.js';
 import { IconManager } from './utils/icon-manager.js';
+import { ROLE_BEHAVIOURS } from './utils/role-behaviours.js';
 
 export class GameState{
     constructor() {
         this.players = []; //lista PlayerClass objekata
         this.pendingRoles = [];  //int index uloge
-        this.nightOrder = []; //redosled id od uloga za budjenje 
         this.lynchVotes = 0;
         this.currentPlayerIndex = 0;
         this.iconManager = new IconManager;
         this.executionTarget = null;
         this.executionerPlayer = null;
+
+        this.nightQueue = []; //redosled roleId, player za budjenje 
+        this.nightIndex = 0;
+
+        this.nightActions = {
+            kills: [], // targets marked for kill
+            bodyguard: new Map(), // investigator.id -> target.id
+            silenced: []
+        };
+
+        this.deaths = [];
+        this.survived = [];
     }
 
     //funkcija za dodavanje igraca
@@ -81,9 +94,16 @@ export class GameState{
     }
 
     addRole(roleId){
-        if(this.pendingRoles.length !== this.players.length){
+        if(this.pendingRoles.length !== this.players.length && this.maxSpecificRole(roleId)){
             this.pendingRoles.push(String(roleId));
         }
+    }
+
+    maxSpecificRole(roleId){
+        if(ROLES[roleId].hasMaximum){
+            return this.getSpecificRoleCount(roleId) < ROLES[roleId].hasMaximum;
+        }
+        else return true
     }
 
     removeRole(roleId){
@@ -137,7 +157,7 @@ export class GameState{
     }
 
     generateExecutionTarget(){
-        const filterExecutioner = this.players.filter(p => p.roleId != ROLE_IDS.DZELAT)
+        const filterExecutioner = this.players.filter(p => p.roleId != ROLE_IDS.DZELAT && p.roleId != ROLE_IDS.LUDAK)
         this.executionTarget = filterExecutioner[Math.floor((Math.random()*filterExecutioner.length))];
     }
 
@@ -160,15 +180,54 @@ export class GameState{
             player.iconPath = icons[index];
         });
 
-        this.nightOrder = this.generateNightOrder(this.players);
+        this.nightQueue = this.buildNightQueue(this.players);
+
+        this.nightIndex = 0;
 
         this.pendingRoles = [];
     }
 
-    generateNightOrder(players){
-        const activeRoleIds = [...new Set(players.map(p => Number(p.roleId)))];
-  
-        return ORDER_OF_ROLES.filter(roleId => activeRoleIds.includes(roleId));
+    buildNightQueue(players) {
+        const roleGroup = new Map();
+
+        players.forEach((player) => {
+            const roleId = Number(player.roleId);
+
+            if(!roleGroup.has(roleId)){
+                roleGroup.set(roleId, []);
+            }
+
+            roleGroup.get(roleId).push(player);
+        })
+
+        this.addAllMafiaToMafiaWakeUp(roleGroup);
+
+        const queue = [];
+
+        ORDER_OF_ROLES.forEach(roleId => {
+
+            if(roleGroup.has(roleId)){
+
+                queue.push({
+                    roleId,
+                    players: roleGroup.get(roleId)
+                });
+
+            }
+
+        });
+
+        return queue;
+    }
+
+    addAllMafiaToMafiaWakeUp(roleGroup){
+        const allUniqueMafia = this.getAllUniqueMafia();
+        if(!roleGroup.has(ROLE_IDS.MAFIJAS) && allUniqueMafia.length > 0){
+            roleGroup.set(ROLE_IDS.MAFIJAS, []);
+        }
+        allUniqueMafia.forEach((player) => {
+            roleGroup.get(ROLE_IDS.MAFIJAS).push(player);
+        })
     }
 
     shuffleArray(array) {
@@ -243,7 +302,7 @@ export class GameState{
 
         this.resetLynch();
 
-        if(topVoted.length > 1) return LYNCH_MESSAGE.TIE(maxVote, listOfNames.slice(0, -1), listOfNames.at(-1));
+        if(topVoted.length > 1) return LYNCH_MESSAGE.TIE(maxVote, listOfNames.slice(0, -1).join(', '), listOfNames.at(-1));
         else{
             topVoted[0].kill();
             return this.checkLynchWinCondition(topVoted[0], maxVote);
@@ -255,9 +314,165 @@ export class GameState{
      */
     getPlayersByAlignment(alignment) {
         return this.players.filter(p => 
-        p.isAlive && p.getAlignment() === alignment
+        p.isAlive && p.getRoleAlignment() === alignment
         );
     }
+
+    getRandomPlayerRole(target){
+        const playersWithoutTarget = this.players.filter(p => p !== target);
+        return ROLES[playersWithoutTarget[Math.floor(Math.random() * playersWithoutTarget.length)].roleId].role;
+    }
+
+    getRandomTownRole(){
+        const townPlayers = this.players.filter(p => p.getRoleAlignment() == ALIGNMENT.TOWN);
+        return ROLES[townPlayers[Math.floor(Math.random() * townPlayers.length)].roleId].role;
+    }
+
+    getRandomMafiaRole(){
+        const mafiaPlayers = this.players.filter(p => p.getRoleAlignment() == ALIGNMENT.MAFIA && p.roleId != ROLE_IDS.KUM);
+        return ROLES[mafiaPlayers[Math.floor(Math.random() * mafiaPlayers.length)].roleId].role;
+    }
+
+    getAllUniqueMafia(){
+        return this.players.filter(p => 
+        p.isAlive && p.isMafiaAligned() && p.roleId != ROLE_IDS.MAFIJAS
+        );
+    }
+
+    isEveryPlayerWithRoleDead(players){
+        return players.every(player => !player.checkIfPlayerAlive());
+    }
+
+    isEveryPlayerWithRoleBlocked(players){
+        return players.every(player => player.checkIfPlayerBlocked());
+    }
+
+    isEveryPlayerActed(players){
+        return players.every(player => player.acted === true);
+    }
+
+    getPlayersByRoleId(roleId){
+        return this.players.filter(p => Number(p.roleId) === roleId);
+    }
+
+    getCurrentNightStep(){
+        return this.nightQueue[this.nightIndex] || null;
+    }
+
+    advanceNight(){
+        this.nightIndex++;
+    }
+
+    isNightFinished(){
+        this.nightIndex >= this.nightQueue.length;
+    }
+
+    calculateNight(){
+        this.deaths = [];
+        this.survived = [];
+        this.applyKills();
+        return this.showNightResults();
+    }
+
+    showNightResults(){
+        let message = ""
+        if(this.nightActions.silenced.length > 0){
+            message += `<p> Igrači koji su ućutkani: ${this.nightActions.silenced.join(', ')}</p>`;
+        }
+        if(this.deaths.length > 0) {
+            message += `<p>Igrači koji su ubijeni: ${this.deaths.join(', ')}</p>`;
+        }
+        if(this.survived.length > 0){
+            message += `<p>Igrači koji su bili napadnuti: ${this.survived.join(', ')}</p>`;
+        }
+        const win = this.checkWinCondition();
+        if(win){
+            message += win;
+        }
+        this.resetNight();
+        if(message.length > 0) return message
+        else return "Noć je bila mirna."
+    }
+
+    resetNight(){
+        this.nightIndex = 0;
+        this.cleanupNightStatuses();
+        this.clearNightActions();
+    }
+
+    clearNightActions() {
+        this.nightActions = {
+            kills: [], 
+            bodyguard: new Map(),
+            silenced: []
+        };
+    }
+
+    getRoleBehaviour(roleId) {
+        return ROLE_BEHAVIOURS[Number(roleId)];
+    }
+
+    setNightButtonAction(id){
+        return ROLE_BEHAVIOURS[id];
+    }
+
+    applyKills() {
+        this.nightActions.kills.forEach(player => {            
+            if (player && player.isAlive) {
+                // Proveri da li je zaštićen
+                if(player.hasStatus(STATUS.IGNITED)){
+                    player.kill();
+                    this.deaths.push(player.name);
+                    console.log(`${player.name} je zapaljen!`);
+                } else if (!player.hasStatus(STATUS.PROTECTED)) {
+                    const bodyguard = this.checkIfBodyguarded(player)
+                    if(bodyguard){
+                        bodyguard.kill();
+                        this.deaths.push(bodyguard.name);
+                        console.log(`${bodyguard.name} je ubijen tokom noći!`);
+                        return;
+                    }
+                    player.kill();
+                    this.deaths.push(player.name);
+                    console.log(`${player.name} je ubijen tokom noći!`);
+                } else {
+                    console.log(`${player.name} je bio zaštićen!`);
+                    this.survived.push(player.name);
+                }
+            }
+        });
+    }
+
+    checkIfBodyguarded(player){
+        for(const [key, value] of this.nightActions.bodyguard){
+            if (value === player){
+                return key;
+            }
+        }
+    }
+
+    cleanupNightStatuses() {
+        this.players.forEach(player => {
+            player.visitedBy = [];
+            player.isBlocked = false;
+            player.visitedByMafia = false;
+            player.acted = false;
+            player.removeStatus(STATUS.PROTECTED);
+            player.removeBodyguardedStatuses();
+            player.removeStatus(STATUS.ATTACK);
+            player.removeStatus(STATUS.TRACKED);
+            player.removeStatus(STATUS.CENSORED);
+            player.removeStatus(STATUS.DUG_UP);
+            player.removeStatus(STATUS.AMESIAC_TARGET);
+            player.removeStatus(STATUS.CONFUSED);
+            player.removeStatus(STATUS.SILENCED);
+            player.removeStatus(STATUS.FALSIFIED);
+            player.removeStatus(STATUS.RECORDED);
+            player.removeStatus(STATUS.LOCKED_UP);
+            player.removeStatus(STATUS.PARASITE_TARGET);
+        });
+    }
+
 
     /**
      * Proveri ko je pobedio
